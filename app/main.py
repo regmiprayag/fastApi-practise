00bad0312d80c7b3d352d0e7e0633ca505aac136
regmiprayag <regmiprayag@gmail.com>
2024-11-15 16:20:04 +0545

    Create update function to update specific post

--- app/main.py
+++ app/main.py
@@ -76,21 +76,22 @@ async def get_post(id: int, response=Response):
 @app.delete("/delete/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_post(id:int):
     # print("The id is: ",id)
-    index = find_index_post(id)
-    if index==None:
+    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING * """,(str(id),))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+    # index = find_index_post(id)
+    if delete_post==None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with this {id} is not found")
-    my_posts.pop(index)
+    # my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/updatepost/{id}")
 async def update_post(id:int,post:Post):
-    print("The post details are: ",post)
-    index = find_index_post(id)
+    # print("The post details are: ",post)
+    cursor.execute("""UPDATE posts SET title=%s, content=%s, published=%s WHERE id = %s RETURNING * """,(post.title,post.content,post.published,str(id),))
+    updated_post = cursor.fetchone()
+    conn.commit()
 
-    if index==None:
+    if updated_post==None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with this {id} is not found")
-    
-    post_dict = post.dict()
-    post_dict['id']=id
-    my_posts[index]=post_dict
-    return {"updated post: ",}
+    return {"data: ", updated_post}
